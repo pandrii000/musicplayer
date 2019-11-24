f00ac6e6ae25b9a169028056bb07785078545cc1
pandrii000 <pandrii000@gmail.com>
2019-11-24 21:42:22 +0200

    home

--- gui.py
+++ gui.py
@@ -1,11 +1,17 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from music import Playlist, MusicPlayer
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
+        self.playlist = Playlist()
+        self.player = MusicPlayer()
 
         self.setWindowTitle("Music Player")
+        self.setMinimumSize(800, 500)
 
         # ======== DEFINE MENU ========  
 
@@ -57,21 +63,112 @@ class MainWindow(QMainWindow):
         # ======== DEFINE HEADER LEVEL ========  
 
         self.song_label = QLabel('No song playing...')
+        self.song_label.setAlignment(Qt.AlignCenter)
         self.main_layout.addWidget(self.song_label)
 
         # ======== DEFINE PLAYLIST LEVEL ========  
 
-        self.playlist_widget = QListWidget()
+        self.playlist_widget = QTableWidget()
+        # adjust size of table to window size
+        self.playlist_widget.horizontalHeader().setStretchLastSection(True)
+        self.playlist_widget.setColumnCount(5)
+        self.playlist_widget.setRowCount(1)
+        self.playlist_widget.setHorizontalHeaderLabels(["Filename", "Artist", "Title", "Format", "Filepath"])
+        self.playlist_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.playlist_widget.horizontalHeaderItem(0).setToolTip("Filename")
+        self.playlist_widget.horizontalHeaderItem(0).setToolTip("Artist")
+        self.playlist_widget.horizontalHeaderItem(1).setToolTip("Title")
+        self.playlist_widget.horizontalHeaderItem(2).setToolTip("Format")
+        self.playlist_widget.horizontalHeaderItem(3).setToolTip("Filepath")
+        self.playlist_widget.doubleClicked.connect(self.on_cell_double_clicked)
         self.main_layout.addWidget(self.playlist_widget)
 
-        # ======== DEFINE BOTTOM LEVEL ========  
+        # ======== DEFINE SLIDER LAYOUT ========  
 
+        self.slider_layout = QHBoxLayout()
+        self.slider_layout_widget = QWidget()
+        self.slider_layout_widget.setLayout(self.slider_layout)
+        self.main_layout.addWidget(self.slider_layout_widget)
+
+        # ======== DEFINE SLIDER LEVEL ======== 
+     
+        self.current_time_label = QLabel('--:--')
+        self.slider_layout.addWidget(self.current_time_label)     
+
+        self.play_slider = QSlider(Qt.Horizontal)
+        self.slider_layout.addWidget(self.play_slider)
+        
+        self.finish_time_label = QLabel('--:--')
+        self.slider_layout.addWidget(self.finish_time_label) 
+
+        # ======== DEFINE CONTROLS LAYOUT ========  
+
+        self.controls_layout = QHBoxLayout()
+        self.controls_layout.setAlignment(Qt.AlignHCenter)
+        self.controls_layout_widget = QWidget()
+        self.controls_layout_widget.setLayout(self.controls_layout)
+        self.main_layout.addWidget(self.controls_layout_widget)
+
+        # ======== DEFINE CONTROLS LEVEL ========
+        
+        self.play_button = QPushButton()
+        self.play_button.setIcon(QIcon('play_button.png'))
+        self.play_button.setIconSize(QSize(30,30))
+        self.play_button.setFixedSize(30, 30)
+        self.play_button.clicked.connect(self.on_play_button_clicked)
+        self.controls_layout.addWidget(self.play_button) 
+
+        self.pause_button = QPushButton()
+        self.pause_button.setIcon(QIcon('pause_button.png'))
+        self.pause_button.setIconSize(QSize(30,30))
+        self.pause_button.setFixedSize(30, 30)
+        self.pause_button.clicked.connect(self.on_pause_button_clicked)
+        self.controls_layout.addWidget(self.pause_button) 
+
+        self.stop_button = QPushButton()
+        self.stop_button.setIcon(QIcon('stop_button.png'))
+        self.stop_button.setIconSize(QSize(30,30))
+        self.stop_button.setFixedSize(30, 30)
+        self.stop_button.clicked.connect(self.on_stop_button_clicked)
+        self.controls_layout.addWidget(self.stop_button) 
+
+        self.repeat_button = QPushButton()
+        self.repeat_button.setIcon(QIcon('repeat_button.png'))
+        self.repeat_button.setIconSize(QSize(30,30))
+        self.repeat_button.setFixedSize(30, 30)
+        self.repeat_button.clicked.connect(self.on_repeat_button_clicked)
+        self.controls_layout.addWidget(self.repeat_button) 
 
     def open_file(self):
-        print("Opening file..")
+        filenames, _ = QFileDialog.getOpenFileNames(self,"Select File", "","Music Files (*.mp3 *.wav)", options=QFileDialog.DontUseNativeDialog)
+        if filenames:
+            for filename in filenames:
+                self.playlist.add_file(filename)
+
+        self.reload_playlist_widget(sorting_category='filename')
+
+    def reload_playlist_widget(self, sorting_category=None):
+        if sorting_category is not None:
+            self.playlist.sort_by(sorting_category)
+
+        # clear widget info
+        while self.playlist_widget.rowCount() > 0:
+            self.playlist_widget.removeRow(0)
+
+        # write info 
+        for i, playlist_element in enumerate(self.playlist):
+            self.playlist_widget.insertRow(i)
+            self.playlist_widget.setItem(i, 0, QTableWidgetItem(playlist_element['filename'])) 
+            self.playlist_widget.setItem(i, 1, QTableWidgetItem(playlist_element['artist'])) 
+            self.playlist_widget.setItem(i, 2, QTableWidgetItem(playlist_element['title'])) 
+            self.playlist_widget.setItem(i, 3, QTableWidgetItem(playlist_element['format'])) 
+            self.playlist_widget.setItem(i, 4, QTableWidgetItem(playlist_element['filepath'])) 
+            print(playlist_element.get_properties())
 
     def open_directory(self):
-        print("Opening directory..")
+        directory = QFileDialog.getExistingDirectory(None, 'Open Folder', '', options=QFileDialog.ShowDirsOnly)
+        if directory:
+            self.playlist.add_from_directory(directory)
 
     def import_playlist(self):
         print("Importing playlist..")
@@ -82,6 +179,26 @@ class MainWindow(QMainWindow):
     def about(self):
         print("About..")
 
+    def on_cell_double_clicked(self, item):
+        index_in_playlist = item.row()
+        playlist_element = self.playlist[index_in_playlist]
+        filepath = playlist_element['filepath']
+        self.player.play(filepath)
+
+    def on_play_button_clicked(self):
+        index_in_playlist = self.playlist_widget.currentRow()
+        playlist_element = self.playlist[index_in_playlist]
+        filepath = playlist_element['filepath']
+        self.player.play(filepath)
+
+    def on_pause_button_clicked(self):
+        self.player.pause()
+
+    def on_stop_button_clicked(self):
+        self.player.stop()
+
+    def on_repeat_button_clicked(self):
+        self.player.repeat = True
 
 
 if __name__ == "__main__":
--- music.py
+++ music.py
@@ -9,10 +9,10 @@ import re
 
 
 # general music pattern
-MUSIC_PATTERN = r'.+\.(mp3|wav)'
+MUSIC_PATTERN = r'[^.]*\.(mp3|wav)'
 
 # Artist - Title .mp3/.wav
-SONG_PATTERN = r'([^\-]+)\-([^\-]+)\.(mp3|wav)'
+SONG_PATTERN = r'([^\-]+)\-([^.]+)\.(mp3|wav)'
 
 
 class PlaylistElement:
@@ -52,10 +52,11 @@ class Playlist:
         element = PlaylistElement(filepath)
         self.elements.append(element)
 
-    def sync_with_directory(self, directory_path):
+    def add_from_directory(self, directory_path):
         for root, dirs, files in os.walk(directory_path):
             for filename in files:
                 if re.match(MUSIC_PATTERN, filename):
+                    print(root, dirs, files)
                     filepath = os.path.join(root, filename)
                     self.add_file(filepath)
 
@@ -69,97 +70,158 @@ class Playlist:
         self.index += 1
         return self.elements[self.index]
 
+    def sort_by(self, category):
+        self.elements.sort(key=lambda el: el[category].lower())
+
+    def __iter__(self):
+        return iter(self.elements)
+
+    def __getitem__(self, index):
+        return self.elements[index ]
+
+
+# class MusicPlayer:
+#     def __init__(self, volume=100):
+#         self.playlist = Playlist()
+#         self.player = pyaudio.PyAudio()
+
+#         self.is_playing = Event()
+#         self.is_playing.clear()
+
+#         self.time_played = 0
+#         self.time_length = 0
+#         self.volume = volume
+
+#         self._plating_thread = Thread(target=self._play)
+#         self._plating_thread.start()
+
+#     def stop(self):
+#         self.time_played = 0
+#         self.is_playing.clear()
+
+#     def pause(self):
+#         self.is_playing.clear()
+    
+#     def play(self):
+#         self.is_playing.set()
+
+#     def _play(self):
+#         # wait for is_playing state
+#         self.is_playing.wait()
+
+#         while self.playlist.has_next():
+#             # wait for is_playing state
+#             self.is_playing.wait()
+
+#             # get next song in playlist
+#             playing_song = self.playlist.get_next()
+
+#             # read sound data
+#             sound = AudioSegment.from_file(playing_song['filepath'])
+
+#             # open stream
+#             stream = self.player.open(
+#                 format=self.player.get_format_from_width(sound.sample_width),
+#                 channels=sound.channels, 
+#                 rate=sound.frame_rate,
+#                 output=True
+#                 )
+
+#             # length of song in seconds for one writing to stream
+#             chunk_length = 50 / 1000
+
+#             # playing song
+#             # self.time_length = self.sound.duration_seconds
+#             # while self.time_played < self.time_length:
+#             #     self.playing.wait()
+#             #     # _data = sound[]
+#             #     self.stream.write(chunk._data)
+#             #     self.time += chunk_length / 1000
+
+#             stream.stop_stream()
+#             stream.close()
+#             # player.terminate()
+
+#     def set_volume(self, value):
+#         """from 0 to 100."""
+#         self.volume = value
+
+
+# def stop_playlist(playlist, time):
+#     sleep(time)
+#     playlist.stop()
+
+
+# def test1():
+#     player = MusicPlayer()
+
+#     playlist = Playlist()
+#     playlist.sync_with_directory('./')
+
+#     player.playlist = playlist
+#     player.play()
+
+#     # while player.playing:
+#     # sleep(1)
+#     # player.set_volume(80)
+#     # print(f"File: {player.playing_song['filename']} Time: {player.time} / {player.song_duration} Volume: {player.volume}")
+
+
+# if __name__ == '__main__':
+#     test1()
+#     # sleep(2)
+#     # sleep(2)
+#     # sleep(2)
+#     # player.stop()
 
 class MusicPlayer:
     def __init__(self, volume=100):
-        self.playlist = Playlist()
         self.player = pyaudio.PyAudio()
 
         self.is_playing = Event()
-        self.is_playing.clear()
+        self.is_playing.set()  
 
-        self.time_played = 0
-        self.time_length = 0
-        self.volume = volume
+        self.repeat = False
+    
+    def play(self, filepath):
+        self.is_playing.set()
 
-        self._plating_thread = Thread(target=self._play)
-        self._plating_thread.start()
+        thread = Thread(target=self._play, args=(filepath,))
+        thread.start()
 
-    def stop(self):
-        self.time_played = 0
-        self.is_playing.clear()
+    def _play(self, filepath):        
+        sound = AudioSegment.from_file(filepath)
+        player = pyaudio.PyAudio()
+    
+        stream = player.open(format = player.get_format_from_width(sound.sample_width),
+            channels = sound.channels,
+            rate = sound.frame_rate,
+            output = True)
+
+        start = 0
+        length = sound.duration_seconds
+        volume = 100.0
+        playchunk = sound[start*1000.0:(start+length)*1000.0] - (60 - (60 * (volume/100.0)))
+        millisecondchunk = 50 / 1000.0
+        
+        time = start
+        for chunks in make_chunks(playchunk, millisecondchunk*1000):
+            self.is_playing.wait()
+
+            time += millisecondchunk
+            stream.write(chunks._data)
+            if time >= start+length:
+                break
+
+        stream.close()
+        player.terminate()
 
     def pause(self):
         self.is_playing.clear()
-    
-    def play(self):
-        self.is_playing.set()
 
-    def _play(self):
-        # wait for is_playing state
-        self.is_playing.wait()
-
-        while self.playlist.has_next():
-            # wait for is_playing state
-            self.is_playing.wait()
-
-            # get next song in playlist
-            playing_song = self.playlist.get_next()
-
-            # read sound data
-            sound = AudioSegment.from_file(playing_song['filepath'])
-
-            # open stream
-            stream = self.player.open(
-                format=self.player.get_format_from_width(sound.sample_width),
-                channels=sound.channels, 
-                rate=sound.frame_rate,
-                output=True
-                )
-
-            # length of song in seconds for one writing to stream
-            chunk_length = 50 / 1000
-
-            # playing song
-            self.time_length = self.sound.duration_seconds
-            while self.time_played < self.time_length:
-                self.playing.wait()
-                _data = sound[]
-                self.stream.write(chunk._data)
-                self.time += chunk_length / 1000
-
-            stream.stop_stream()
-            stream.close()
-            # player.terminate()
+    def stop(self):
+        self.is_playing.clear()
 
     def set_volume(self, value):
         """from 0 to 100."""
         self.volume = value
-
-
-def stop_playlist(playlist, time):
-    sleep(time)
-    playlist.stop()
-
-
-def test1():
-    player = MusicPlayer()
-
-    playlist = Playlist()
-    playlist.sync_with_directory('./')
-
-    player.playlist = playlist
-    player.play()
-
-    # while player.playing:
-    # sleep(1)
-    # player.set_volume(80)
-    # print(f"File: {player.playing_song['filename']} Time: {player.time} / {player.song_duration} Volume: {player.volume}")
-
-
-if __name__ == '__main__':
-    test1()
-    # sleep(2)
-    # sleep(2)
-    # sleep(2)
-    # player.stop()
